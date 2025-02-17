ed5a205a9a5dfb20326487f51f6bf3a5bf5b53e9
Himanshu Joshi <hiimanshuhh16@gmail.com>
2025-02-17 10:58:15 +0530

    updating

---
+++ components/header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+import Image from 'next/image'
+
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <Link href="/">
+          <Image src="/logo.png"
+                 alt="Sensai Logo" 
+                 width={200} 
+                 height={60} 
+                 className=" h-12 py-1 w-auto object-contain"/>
+        </Link>
+      </nav>
+      <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+    </header>
+  )
+}
+
+export default Header
