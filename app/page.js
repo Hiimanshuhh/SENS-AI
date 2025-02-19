029e25d0657e35f3b2186e4bf861ef1fb4ab1458
Himanshu Joshi <hiimanshuhh16@gmail.com>
2025-02-19 16:23:33 +0530

    updating

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import HeroSection from "@/components/hero";
 
 
 export default function Home() {
   return (
-    <div>let's make this project and learn everything</div>
+    <div>
+      <div className="grid-background"></div>
+      <HeroSection/>
+    </div>
   );
 }
